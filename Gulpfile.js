849379bcf3a3157defec5852c2a0dd9dcca9f32f
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-07-10 21:58:58 -0600

    Add a gulpfile to do the heavy lifting

---
+++ Gulpfile.js
@@ -0,0 +1,41 @@
+var gulp   = require('gulp'),
+    source = require('vinyl-source-stream'),
+    clean  = require('gulp-clean');
+
+gulp.task('clean:js', function () {
+    return gulp.src('./dist/bundle.js', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('hint', function () {
+    var jshint  = require('gulp-jshint'),
+        stylish = require('jshint-stylish');
+
+    return gulp.src(['./src/**/*.js', './demo/**/*.js', './Gulpfile.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('js', ['clean:js'], function () {
+    var browserify = require('browserify');
+
+    return browserify('./demo/js/index.js')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('copy', function () {
+    return gulp.src('./demo/index.html')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', ['hint', 'js', 'copy']);
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch(['./src/**/*.js', './demo/**/*.js', './Gulpfile.js'], ['hint']);
+    gulp.watch(['./src/**/*.js', './demo/**/*.js'], ['js']);
+    gulp.watch('./demo/*.html', ['copy']);
+});
+
+gulp.task('default', ['build']);
