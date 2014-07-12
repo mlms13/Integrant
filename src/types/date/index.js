2e9965d1e7322a32f8787af8daa9ccb379348165
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-07-12 16:58:32 -0600

    Actually add a date validation function

--- src/types/date/index.js
+++ src/types/date/index.js
@@ -1,4 +1,8 @@
+var _ = require('lodash');
 
 module.exports = {
-    template: require('./date.jade')
+    template: require('./date.jade'),
+    validate: function (input) {
+        return _.isDate(input);
+    }
 };
